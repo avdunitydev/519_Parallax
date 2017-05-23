400a88cdade9b745477aed41274a10e8d389d64d
avdunitydev <avdunitydev@users.noreply.github.com>
2017-05-23 15:17:30 +0300

    523CW

---
+++ Assets/1_Particle/CubeLife.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CubeLife : MonoBehaviour
+{
+	public float HP;
+
+
+	// Use this for initialization
+	void Awake ()
+	{
+		HP = 30;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnParticleCollision (GameObject go)
+	{
+		if (HP <= 0) {
+			Destroy (gameObject);
+		} else {
+			--HP;
+		}
+	}
+}
---
+++ Assets/1_Particle/ParticalEfect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ParticalEfect : MonoBehaviour
+{
+	void OnParticleTrigger ()
+	{
+	}
+
+	void OnParticleCollision (GameObject other)
+	{
+		ParticleSystem ps = GetComponent<ParticleSystem> ();
+		List<ParticleCollisionEvent> collEvent1 = new List<ParticleCollisionEvent> ();
+		List<ParticleCollisionEvent> collEvent2 = new List<ParticleCollisionEvent> ();
+		List<ParticleCollisionEvent> collEvent3 = new List<ParticleCollisionEvent> ();
+		
+		if (other.name.Equals ("Cube (0)")) {
+			int numCollisionEvents = ps.GetCollisionEvents (other, collEvent1);
+			Material mat = other.GetComponent<Material> ();
+
+			int i = 0;
+
+			while (i < numCollisionEvents) {
+				mat.color = new Color (Random.Range (0, 250), Random.Range (0, 250), Random.Range (0, 250), 0);
+				i++;
+			}
+		}
+		
+		
+	}
+
+}
